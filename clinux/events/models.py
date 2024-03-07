28ace89bc2b4e79a8225c8d3ab0784ebc8ac0717
Lick1Fonzi <dbianchini21@gmail.com>
2024-03-08 00:07:59 +0100

    models of events

--- clinux/events/models.py
+++ clinux/events/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+
+class EventSession(models.Model):
+    event = models.ForeignKey(
+        "Event",
+        on_delete=models.CASCADE,
+        related_name="sessions",
+        null=False,
+        blank=False,
+    )
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+
+
 # Create your models here.
+
+
+class Event(models.Model):
+    slug = models.SlugField(unique=True, max_length=100)
+    title = models.CharField(null=False, blank=False)
+    subtitle = models.CharField(default="", blank=True)
+    description = models.TextField(default="", blank=True)
+
+    # TO_DO: timestamp
+    # hours = models.CharField()
+    # level = models.CharField()
+    # image = models.ImageField(upload_to = user_directory_path)
+    @property
+    def start(self):
+        return min(s.start for s in self.sessions)
+
+    @property
+    def end(self):
+        return max(s.end for s in self.sessions)
