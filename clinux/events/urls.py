4471a747bcae4bb8562fc9e6c115776b6d5082a4
Umuril Lyerood <umuril.lyerood@gmail.com>
2024-03-14 21:37:15 +0100

    Add event list page

--- clinux/events/urls.py
+++ clinux/events/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
 from . import views
 
-app_name = 'events'
+app_name = "events"
 
 urlpatterns = [
-    #path("", views.index, name="index"),
+    # path("", views.index, name="index"),
+    path("", views.EventListView.as_view(), name="event-list"),
 ]
--- clinux/events/views.py
+++ clinux/events/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.views.generic.list import ListView
+
+from . import models
 
 # Create your views here.
+
+
+class EventListView(ListView):
+    model = models.Event
+    paginate_by = 5  # if pagination is desired
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = timezone.now()
+        return context
