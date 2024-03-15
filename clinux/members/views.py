b6863aae9bc37c383958e6b1b9bae76f6605ec89
Elia Giacobazzi (graphene) <elia.giacobazzi@gmail.com>
2024-03-15 03:33:20 +0100

    Add basic Member view and template

--- clinux/members/views.py
+++ clinux/members/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+from .models import Member
+
+
+class MemberDetailView(DetailView):
+    model = Member
+    context_object_name = "member"
+    template_name = "members/member.html"
