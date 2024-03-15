a500df6661af7c2e433e6ed915d89e8b8c7b64b8
Elia Giacobazzi (graphene) <elia.giacobazzi@gmail.com>
2024-03-15 03:21:42 +0100

    Add Member model

--- clinux/members/admin.py
+++ clinux/members/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Member
+
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = [
+        "full_name",
+        "first_name",
+        "last_name",
+        "social_id",
+        "gender",
+        "birth_date",
+    ]
--- clinux/members/models.py
+++ clinux/members/models.py
@@ -1,3 +1,82 @@
+from codicefiscale import codicefiscale
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
 from django.db import models
+from django.shortcuts import reverse
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Gender(models.TextChoices):
+    MALE = "M", _("Male")
+    FEMALE = "F", _("Female")
+    OTHER = "O", _("Other")
+
+
+def check_italian_social_id(code: str):
+    """Check if an italian social id is formally valid"""
+    if not codicefiscale.is_valid(code):
+        raise ValidationError(_(f"Social ID {code} is formally invalid"))
+
+
+class Member(models.Model):
+    """A member of the association"""
+
+    # TODO: before adding user it's better to create custom User class
+    # user = models.OneToOneField(
+    #     settings.AUTH_USER_MODEL,
+    #     on_delete=models.PROTECT,
+    #     verbose_name=_("User"),
+    #     related_name="member",
+    # )
+
+    first_name = models.CharField(_("Name"), max_length=100)
+    last_name = models.CharField(_("Surname"), max_length=100)
+
+    social_id = models.CharField(
+        _("Social ID"),
+        max_length=16,
+        validators=[check_italian_social_id],
+    )
+
+    gender = models.CharField(_("Gender"), max_length=1, choices=Gender.choices)
+
+    birth_date = models.DateField(_("Birth Date"))
+    birth_city = models.CharField(
+        _("Birth City"),
+        max_length=150,
+        help_text=_("City/municipality or foreign country where Member is born"),
+    )
+    # TODO: Province/Region require a separate structure
+    # birth_province = models.ForeignKey(
+    #     Region,
+    #     on_delete=models.PROTECT,
+    #     verbose_name=_("Birth Province"),
+    #     help_text=_("Birth Region (EE for other countries)"),
+    # )
+    # birth_country = models.CharField(_("Birth Country"), max_length=...)
+
+    phone = models.CharField(
+        _("Phone Number"),
+        max_length=50,
+        help_text=_("Phone Number, use only digits, +, -, space and parenthesis"),
+        validators=[
+            RegexValidator(
+                r"^(00|\+)?((\d+|\(\d+\))[ \-]?)+\d$",
+                _("Use only plus sign (at start), dashes (-), spaces and parenthesis"),
+            )
+        ],
+    )
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    def get_absolute_url(self):
+        return reverse("member", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return f"{self.full_name} ({self.social_id})"
+
+    class Meta:
+        verbose_name = _("Member")
+        verbose_name_plural = _("Members")
