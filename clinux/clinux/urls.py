ba68201318adf6f51c15e7df7cb9da1f04ae8f16
Michele Mosca <michele.mosca026@gmail.com>
2024-03-21 23:39:15 +0100

    A basic template with header, content and footer blocks is created

--- clinux/clinux/urls.py
+++ clinux/clinux/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("web/", include("website.urls")),
     path("events/", include("events.urls")),
     path("members/", include("members.urls")),
+    path("", include("website.urls")),
 ]
