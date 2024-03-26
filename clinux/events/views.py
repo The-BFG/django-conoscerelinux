3c213580c06c463ea0ee4c46f5b3bd93a4c1f1fb
Elia Giacobazzi (graphene) <elia.giacobazzi@gmail.com>
2024-03-27 00:21:40 +0100

    Remove unused get_context_data

--- clinux/events/views.py
+++ clinux/events/views.py
@@ -9,20 +9,10 @@ class EventDetailView(DetailView):
     model = models.Event
     context_object_name = "event"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["now"] = timezone.now()
-        return context
-
 
 class EventSessionDetailView(DetailView):
     model = models.EventSession
     context_object_name = "session"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["now"] = timezone.now()
-        return context
 
 
 class EventListView(ListView):
@@ -30,18 +20,8 @@ class EventListView(ListView):
     context_object_name = "events"
     paginate_by = 5  # if pagination is desired
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["now"] = timezone.now()
-        return context
-
 
 class EventSessionListView(ListView):
     model = models.EventSession
     context_object_name = "sessions"
     paginate_by = 5  # if pagination is desired
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["now"] = timezone.now()
-        return context
