e8cce1f936cec71ec5ca1f840eaafb9066c1ebc8
Elia Giacobazzi (graphene) <elia.giacobazzi@gmail.com>
2024-03-26 22:03:52 +0100

    Cleanup authentication app

--- clinux/authentication/models.py
+++ clinux/authentication/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 
+# NOTE: A good practice in Django is to create a custom User class ass soon as possible
+#       This is caused by the fact that creating a custom user after creation of
+#       migrations couse CircularDependency errors
+#       For more info: https://docs.djangoproject.com/en/5.0/topics/auth/customizing/
 
-# Create your models here.
+
 class User(AbstractUser):
     pass
